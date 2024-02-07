846cafa8d6646ab7008b394cacb2391f37539baa
Bruno Henrique <brunodorea@outlook.com.br>
2024-02-07 09:38:21 -0300

    :sparkles: feat: implementando view para tag_creator com Http Types

---
+++ src/views/tag_creator_view.py
@@ -0,0 +1,17 @@
+from src.views.http_types.http_request import HttpRequest
+from src.views.http_types.http_response import HttpResponse
+
+class TagCreatorView:
+    '''
+        responsabilidade para interagir com HTTP
+    '''
+    def validate_and_create(self, http_request: HttpRequest):
+        # body = http_request.body
+        # product_code = body['product_code']
+
+        # regras de negocio
+        print('estou na view')
+        print(http_request)
+
+        # retorno http
+        return HttpResponse(status_code=200, body={'hello': 'world'})
